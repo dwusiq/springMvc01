623b7a96ae02f36d9900f1f54d66a530b3ed77bf
dwusiq <dwusiq@163.com>
2017-06-20 11:38:46 +0800

    增加参数校验功能

--- src/main/java/com/wusiq/controller/LoginController.java
+++ src/main/java/com/wusiq/controller/LoginController.java
@@ -4,11 +4,15 @@ import com.wusiq.entity.ReqAddUserEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
 
 /**
  * 控制器
@@ -44,8 +48,22 @@ public class LoginController {
      *接收表单请求
      */
     @RequestMapping(value = "/addUser.do",method = RequestMethod.POST)
-    public String addUser(ReqAddUserEntity req){
+    public String addUser(@Validated ReqAddUserEntity req, BindingResult result){
         LOGGER.info("ReqAddUserEntity:{}",req);
+
+        if(result.hasErrors()){
+            LOGGER.warn("参数错误：{}",result.getAllErrors());
+            return "redirect:/loginController/toErrorPage.do";
+        }
         return "welcomePage";
+    }
+
+    /***
+     *跳转错误页
+     */
+    @RequestMapping(value = "/toErrorPage.do",method = RequestMethod.GET)
+    public String toErrorPage(){
+        LOGGER.info("失败了!");
+        return "errorPage";
     }
 }
--- src/main/java/com/wusiq/entity/ReqAddUserEntity.java
+++ src/main/java/com/wusiq/entity/ReqAddUserEntity.java
@@ -1,10 +1,17 @@
 package com.wusiq.entity;
 
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.constraints.Size;
+
 /**
  * 添加用户，页面请求的实体类
  */
 public class ReqAddUserEntity {
+    @NotEmpty(message = "姓名不能为空")
     private String userName;//用户名
+    @Size(min = 2,max = 30,message = "用户地址长度应为{min}-{max}位")
     private String userFrom;//用户来自于
 
     public String getUserName() {
