f9c729a3074685a7f20a2194a705bb1049dd6df7
dwusiq <dwusiq@163.com>
2017-07-28 14:39:33 +0800

    去除多余引用

--- src/main/java/com/wusiq/controller/LoginController.java
+++ src/main/java/com/wusiq/controller/LoginController.java
@@ -5,14 +5,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.validation.Valid;
 
 /**
  * 控制器
